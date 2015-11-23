e18e7582a08ac561967ee0bd887150bd73fe0036
Alexandra Leisse <troubalex@gmail.com>
2015-11-23 11:06:44 +0100

    Adds working menu toggle without any further styling. Moving on for now.

--- source/assets/javascripts/_menu-toggle.js
+++ source/assets/javascripts/_menu-toggle.js
@@ -1,1 +1,6 @@
+$('.menu-toggle').bind('click', function(e){
+  jQuery(this).parent().find('.main-navigation').slideToggle('fast');  // apply the toggle to the ul
+  jQuery(this).parent().toggleClass('is-visible');
+  e.preventDefault();
+});
 
