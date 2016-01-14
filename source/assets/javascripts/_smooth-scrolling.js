cedffad3a76363327f04bf9af36bb19bff537135
Alexandra Leisse <troubalex@gmail.com>
2016-01-14 17:58:34 +0100

    Added smooth scrolling for anchor links.
    Special thanks to @barlindhaug :heart:

---
+++ source/assets/javascripts/_smooth-scrolling.js
@@ -0,0 +1,6 @@
+$('#anchor-button a').click(function(event) {
+  event.preventDefault();
+
+  var anchorID = $(this).attr("href");
+  $("body").animate({scrollTop: $(anchorID).offset().top}, "slow");
+});
